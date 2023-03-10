ab97e130ad0b1a1b9ed17a273bba218ee4cd9d2a
Valentin <valentin.druelle@unibas.ch>
2023-03-10 17:06:19 +0100

    Calibration for pumps. Need testing

--- scripts/calibration.py
+++ scripts/calibration.py
@@ -14,7 +14,7 @@ def calibrate_OD(
         vials: list = list(range(1, 16)),
         nb_measures: int = 10,
         lag: float = 0.1,
-        voltage_threshold: float = 4.8) -> np.ndarray:
+        voltage_threshold: float = 4.8) -> None:
     """Function to perform the calibration of the OD measurment. It is done by measuring some standard with
     known OD in each of the vial slots. It is suggested to use 4 standards that span the whole range of
     measurable OD.
@@ -40,13 +40,10 @@ def calibrate_OD(
     for ii in range(nb_standards):  # iterating over all standards
         no_valid_standard = True
         while no_valid_standard:
-            try:
-                print()
-                cur_OD = input(f"Enter OD of standard {ii+1}: ")
-                ODs.append(float(cur_OD))
-                no_valid_standard = False
-            except BaseException:
-                print("invalid entry")
+            print()
+            cur_OD = input(f"Enter OD of standard {ii+1}: ")
+            ODs.append(float(cur_OD))
+            no_valid_standard = False
 
         for jj, vial in enumerate(vials):  # iterating over all vials for the current OD standard
             input("   Place OD standard in vial slot " + str(vial) + ", press enter when done")
@@ -58,7 +55,7 @@ def calibrate_OD(
     print()
     print("Calibration manipulation complete. Calculating voltage -> OD conversion.")
     ODs = np.array(ODs)
-    fit_parameters = np.zeros((len(vials), 2))
+    fit_parameters = np.zeros((len(vials), 2))  # first columns are slope, second are intercepts
 
     # Computing the fit for all vials
     for ii, vial in enumerate(vials):
@@ -81,7 +78,43 @@ def calibrate_OD(
     plt.ylabel('Voltage [V]')
     plt.show()
 
-    return fit_parameters
+
+def calibrate_pumps(interface: Interface, filename: str, pumps: list, dt: float = 10):
+    """Function to perform the calibration of the pumps. For now it is done by connecting all the pumps and
+    putting their inlet in water and their outlet on the scale. Then each pump is run for dt seconds and the
+    user is asked for the new weight. Pumping rates are inferred from the differenc in weights.
+
+    Args:
+        interface: Interface object from interface.py file.
+        filename: name of the save file.
+        pumps: list of the pumps to calibrate.
+        dt: Pumping time for the calibration. Defaults to 10.
+    """
+    weights = np.zeros((2, len(pumps)))  # Pumps are columns, first line is weight before and second is after
+
+    print(f"Starting pump calibration for pumps {pumps}.")
+    print("Put inlet of all pumps in water. Put outlet of pumps in vial on a balance")
+
+    input("When the setup is ready press enter. It will run all the pumps for 10s to fill the tubing.")
+    for pump in enumerate(pumps):
+        interface._run_pump(pump, 10)
+
+    weight = input("Initial weight of vial: ")
+    for ii, pump in enumerate(pumps):  # iterate over all pumps and asks for weights
+        print(f"Calibrating pump {pump}:")
+        weights[0, ii] = weight
+        interface._run_pump(pump, dt)
+        weight = input("    Measured weight of vial: ")
+        weights[1, ii] = weight
+
+    print()
+    print("Calibration manipulation complete. Computing pumping rate.")
+
+    # calculate pump_rate and save to file
+    pump_rates = (weight[1,:] - weight[0,:]) / dt
+
+    print(f"Saving data in {filename}.")
+    np.savetxt(filename, pump_rates)
 
 
 if __name__ == "__main__":
