7fb38fb3755f7cee8216159b04df26330e9b9a0b
Valentin <valentin.druelle@unibas.ch>
2023-03-21 15:07:31 +0100

    Finished and tested calibration

--- scripts/calibration.py
+++ scripts/calibration.py
@@ -37,6 +37,7 @@ def calibrate_OD(
 
     ODs = []
     voltages = np.zeros((nb_standards, len(vials)))
+    print()
 
     # Measuring the voltage for all vial + OD standard combo
     for ii in range(nb_standards):  # iterating over all standards
@@ -81,46 +82,6 @@ def calibrate_OD(
     plt.show()
 
 
-def calibrate_pumps(interface: Interface, filename: str, pumps: list, dt: float = 30):
-    """Function to perform the calibration of the pumps. For now it is done by connecting all the pumps and
-    putting their inlet in water and their outlet on the scale. Then each pump is run for dt seconds and the
-    user is asked for the new weight. Pumping rates are inferred from the difference in weights.
-    Saves the pumping rate in a file with the given filename.
-
-    Args:
-        interface: Interface object from interface.py file.
-        filename: name of the save file.
-        pumps: list of the pumps to calibrate.
-        dt: Pumping time for the calibration in seconds. Defaults to 10.
-    """
-    weights = np.zeros((2, len(pumps)))  # Pumps are columns, first line is weight before and second is after
-
-    print(f"Starting pump calibration for pumps {pumps}.")
-    print("Put inlet of all pumps in water. Put outlet of pumps in vial on a balance")
-
-    input("When the setup is ready press enter. It will run all the pumps for 20s to fill the tubing.")
-    for pump in pumps:
-        print(f"Running pump {pump}")
-        interface._run_pump(pump, 20)
-
-    weight = input("Initial weight of vial: ")
-    for ii, pump in enumerate(pumps):  # iterate over all pumps and asks for weights
-        print(f"Calibrating pump {pump}")
-        weights[0, ii] = weight
-        interface._run_pump(pump, dt)
-        weight = input("    Measured weight of vial: ")
-        weights[1, ii] = weight
-
-    print()
-    print("Calibration manipulation complete. Computing pumping rate.")
-
-    # calculate pump_rate and save to file
-    pump_rates = (weights[1, :] - weights[0, :]) / dt  # g.s^-1 <=> mL*s^-1
-
-    print(f"Saving data in {filename}.")
-    np.savetxt(CALI_PATH + filename, pump_rates)
-
-
 def calibrate_weight_sensors(
         interface: Interface,
         filename: str,
@@ -151,6 +112,7 @@ def calibrate_weight_sensors(
 
     assert len(pump_times) > 2, "Cannot calibrate weight sensors with less than 2 pumping times."
 
+    print()
     print(f"Starting calibration for weight sensors of vials {vials}.")
     input(f"Put the inlet and outlet of pump {pump} in water to pre-fill the tubes, then press enter.")
 
@@ -173,7 +135,7 @@ def calibrate_weight_sensors(
             time.sleep(1)
             voltages[jj + 1, ii] = interface.measure_weight(vial)  # after pumping
             tot_time = t
-            print(f"    Measuring voltage after {tot_time}: {voltages[jj+1, ii]}")
+            print(f"    Measuring voltage after {tot_time}s: {voltages[jj+1, ii]}")
 
         print(f"Calibration of weight sensor {vial} done.")
 
@@ -203,42 +165,96 @@ def calibrate_weight_sensors(
     plt.show()
 
 
-def group_calibrations(cali_OD: str, cali_pumps: str, cali_WS: str, output: str):
-    fits_OD = np.load(CALI_PATH + cali_OD)
-    rate_pumps = np.load(CALI_PATH + cali_pumps)
-    fits_WS = np.load(CALI_PATH + cali_WS)
+def calibrate_pumps(interface: Interface, filename: str, pumps: list, dt: float = 30):
+    """Function to perform the calibration of the pumps. For now it is done by connecting all the pumps and
+    putting their inlet in water and their outlet on the scale. Then each pump is run for dt seconds and the
+    user is asked for the new weight. Pumping rates are inferred from the difference in weights.
+    Saves the pumping rate in a file with the given filename.
+
+    Args:
+        interface: Interface object from interface.py file.
+        filename: name of the save file.
+        pumps: list of the pumps to calibrate.
+        dt: Pumping time for the calibration in seconds. Defaults to 10.
+    """
+    weights = np.zeros((2, len(pumps)))  # Pumps are columns, first line is weight before and second is after
+
+    print()
+    print(f"Starting pump calibration for pumps {pumps}.")
+    print("Put inlet of all pumps in water. Put outlet of pumps in vial on a balance")
+
+    input("When the setup is ready press enter. It will run all the pumps for 20s to fill the tubing.")
+    for pump in pumps:
+        print(f"Running pump {pump}")
+        interface._run_pump(pump, 20)
+
+    weight = input("Initial weight of vial: ")
+    for ii, pump in enumerate(pumps):  # iterate over all pumps and asks for weights
+        print(f"Calibrating pump {pump}")
+        weights[0, ii] = weight
+        interface._run_pump(pump, dt)
+        weight = input("    Measured weight of vial: ")
+        weights[1, ii] = weight
+
+    print()
+    print("Calibration manipulation complete. Computing pumping rate.")
+
+    # calculate pump_rate and save to file
+    pump_rates = (weights[1, :] - weights[0, :]) / dt  # g.s^-1 <=> mL*s^-1
+
+    print(f"Saving data in {filename}.")
+    np.savetxt(CALI_PATH + filename, pump_rates)
+
+
+def group_calibrations(cali_OD: str, cali_WS: str, cali_pumps: str, output: str):
+    fits_OD = np.loadtxt(CALI_PATH + cali_OD)
+    fits_WS = np.loadtxt(CALI_PATH + cali_WS)
+    rate_pumps = np.loadtxt(CALI_PATH + cali_pumps)
+
+    print()
+    print(f"Concatenating {cali_OD}, {cali_WS} and {cali_pumps}")
+
+    calibration_dict = {}
 
     cali_OD = {}
     for ii in range(fits_OD.shape[0]):
-        pass
+        cali_OD[f"vial {ii}"] = {"slope": {"value": float(fits_OD[ii, 0]), "units": "V.OD^-1"},
+                                 "intercept": {"value": float(fits_OD[ii, 1]), "units": "V"}}
+    calibration_dict["OD"] = cali_OD
+
+    cali_WS = {}
+    for ii in range(fits_WS.shape[0]):
+        cali_WS[f"vial {ii}"] = {"slope": {"value": float(fits_WS[ii, 0]), "units": "V.g^-1"},
+                                 "intercept": {"value": float(fits_WS[ii, 1]), "units": "V"}}
+    calibration_dict["WS"] = cali_WS
+
+    cali_pumps = {}
+    for ii in range(rate_pumps.shape[0]):
+        cali_pumps[f"pump {ii}"] = {"rate": {"value": float(rate_pumps[ii]), "units": "mL.s^-1"}}
+    calibration_dict["pumps"] = cali_pumps
+
+    with open(CALI_PATH + output + ".yaml", "w") as f:
+        yaml.dump(calibration_dict, f)
+    
+    print(f"Calibration saved in {output}.yaml")
 
 
 if __name__ == "__main__":
     try:
         interface = Interface()
-        # calibrate_OD(interface, "OD.npy", nb_standards=3, vials=[1, 2])
-        # calibrate_pumps(interface, "pumps.npy", [1,2])
-        # calibrate_weight_sensors(interface, "WS.npy", vials=[1, 2])
-        # group_calibrations("OD.npy", "pumps.npy", "WS.npy", "14-03.yml")
-
-        cali_OD = "OD.txt"
-        cali_pumps = "pumps.txt"
-        cali_WS = "WS.txt"
-        fits_OD = np.loadtxt(cali_OD)
-        rate_pumps = np.loadtxt(cali_pumps)
-        # fits_WS = np.loadtxt(cali_WS)
-
-        calibration_dict = {"OD": {}, "pumps": {}, "WS": {}}
-
-        cali_OD = {}
-        for ii in range(fits_OD.shape[0]):
-            cali_OD[f"vial {ii}"] = {"slope": fits_OD[ii, 0], "intercept": fits_OD[ii, 1]}
-        calibration_dict["OD"] = cali_OD
-
-        cali_pumps = {}
-        for ii in range(rate_pumps.shape[0]):
-            cali_pumps[f"pump {ii}"] = {"rate": rate_pumps[ii]}
-        calibration_dict["pumps"] = cali_pumps
+        choice = input("What would you like to calibrate ? [OD, pumps, WS, concatenate]: ")
+        if choice == "OD":
+            calibrate_OD(interface, "OD.txt", nb_standards=5, vials=[1, 2])
+        elif choice == "pumps":
+            calibrate_pumps(interface, "pumps.txt", [1, 2])
+        elif choice == "WS":
+            calibrate_weight_sensors(interface, "WS.txt", vials=[1, 2])
+        elif choice == "concatenate":
+            t = time.localtime()
+            date_string = f"{format(t.tm_mon, '02d')}-{format(t.tm_mday, '02d')}-{format(t.tm_hour,'02d')}h-{format(t.tm_min, '02d')}min"
+            group_calibrations("OD.txt", "WS.txt", "pumps.txt", date_string)
+        else:
+            print(f"{choice} is not in the available actions: [OD, pumps, WS, concatenate]")
 
     finally:  # This executes among program exiting (error or pressing ctrl+C)
         interface.turn_off()
