dc349d0bcb0bbdaa2876f736a7fbf217dd819d52
Valentin <valentin.druelle@unibas.ch>
2023-03-29 14:04:43 +0200

    test async from chatgpt

--- scripts/test.py
+++ scripts/test.py
@@ -1,1 +1,16 @@
 # Used to make some small tests for the RPi control
+import asyncio
+
+async def say_hello(text):
+    await asyncio.sleep(10)
+    print(text)
+
+async def main():
+    tasks = []
+    for i in range(5):
+        task = asyncio.create_task(say_hello("hello"))
+        tasks.append(task)
+    await asyncio.gather(*tasks)
+
+if __name__ == "__main__":
+    asyncio.run(main())
