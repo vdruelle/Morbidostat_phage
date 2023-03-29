d3535a0be4bf1a927ec1feb8bb1742694a449b10
Valentin <valentin.druelle@unibas.ch>
2023-03-29 16:51:02 +0200

    minimal asyn test that seems usable

--- scripts/test.py
+++ scripts/test.py
@@ -2,14 +2,18 @@
 import asyncio
 
 async def say_hello(text):
+    await asyncio.sleep(text)
     print(text)
-    await asyncio.sleep(2)
 
 async def main():
-    tasks = []
-    for i in range(5):
-        task = asyncio.create_task(say_hello("hello"))
-        tasks.append(task)
+    for ii in range(5):
+        asyncio.create_task(say_hello(ii))
+    tasks = [task for task in asyncio.all_tasks() if task is not asyncio.current_task()]
+
+    print(asyncio.all_tasks())
+    await asyncio.sleep(2.5)
+    print(asyncio.all_tasks())
+    
     await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
