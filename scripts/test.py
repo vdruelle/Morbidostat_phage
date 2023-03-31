ab6db068872c83fab8ab81051eea98b1f112e4b8
Valentin <valentin.druelle@unibas.ch>
2023-03-31 14:12:14 +0200

    asyn task list minimal example

--- scripts/test.py
+++ scripts/test.py
@@ -1,44 +1,28 @@
 # Used to make some small tests for the RPi control
 import asyncio
-import time
-from interface import Interface
 
-async def say_hello(dt):
-    await asyncio.sleep(dt)
-    print("Hello")
 
-async def run_hello_tasks(tasks):
-    await asyncio.gather(*tasks)
+class Interface:
+    def __init__(self) -> None:
+        self.tasks = []
+        self.loop = asyncio.get_event_loop()
 
-def run_blocking_function():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.gather(*asyncio.all_tasks()))
-    print("Done")
+    def add_pumping(self, pump, dt):
+        async def _pump_coroutine(pump, dt):
+            print(f"Starting pump {pump}.")
+            await asyncio.sleep(dt)
+            print(f"Pump {pump} finished after {dt} seconds.")
 
-if __name__ == '__main__':
-    # The loop seems to allow to put events there, and start them only when the run_until_complete function
-    # is called.
-    loop = asyncio.get_event_loop()
+        self.tasks.append(asyncio.ensure_future(_pump_coroutine(pump, dt)))
 
+    def launch_pump(self):
+        self.loop.run_until_complete(asyncio.gather(*self.tasks))
+
+
+if __name__ == "__main__":
     tmp = Interface()
-    task1 = loop.create_task(tmp._run_pump(1, 5))
-    task2 = loop.create_task(tmp._run_pump(2, 10))
     print("test")
-    time.sleep(3)
+    tmp.add_pumping(1, 2)
+    tmp.add_pumping(2, 4)
+    tmp.launch_pump()
     print("test2")
-    # tasks = [task for task in asyncio.all_tasks() if task is not asyncio.current_task()]
-    loop.run_until_complete(asyncio.gather(task1, task2))
-    print("test3")
-
-    # First batch of tasks
-    # tasks1 = [loop.create_task(say_hello(2)) for _ in range(3)]
-
-    # Do other things in your code here...
-
-    # Second batch of tasks
-    # tasks2 = [loop.create_task(say_hello(4)) for _ in range(2)]
-
-    # Do other things in your code here...
-
-    # Wait for all tasks to complete
-    # loop.run_until_complete(run_hello_tasks(tasks1 + tasks2))
