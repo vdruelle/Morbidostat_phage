5aac5e6adde6eae11168b34710aeee43ff650e69
Valentin <valentin.druelle@unibas.ch>
2023-06-06 14:48:23 +0200

    Uploda before working from home

--- scripts/test_hardware.py
+++ scripts/test_hardware.py
@@ -1,7 +1,9 @@
+import time
+
+import RPi.GPIO as GPIO
 import smbus
 from hardware_libraries import ADCPi, IOPi
 from interface import Interface
-import RPi.GPIO as GPIO
 
 
 def test_I2C_connections():
@@ -25,7 +27,7 @@ def test_I2C_connections():
     print()
 
 
-def test_ADCPi(threshold_voltage=0.05, light_pin = 20):
+def test_ADCPi(threshold_voltage=0.05, light_pin=20):
     """Reads voltage from all the pins of all the ADCPis. Perform this test with empty vials in place.
     Write as output which pin got a voltage reading that is below threshold.
     """
@@ -57,6 +59,55 @@ def test_ADCPi(threshold_voltage=0.05, light_pin = 20):
     print()
 
 
+def test_pump_sound(run_time=1, interval_time=0.5):
+    """Turns pump on and off to check if they are responsive to the RPi. Perform this test while listening
+    to the pump noise.
+
+    Args:
+        run_time: Run time of each pumps in seconds. Defaults to 1.
+    """
+    IOPis = [IOPi(0x20)]
+    for iopi in IOPis:
+        for ii in range(1, 17):
+            iopi.set_pin_direction(ii, 0)
+
+    print("Performing sequential pumping")
+    for iopi in IOPis:
+        for ii in range(1, 17):
+            iopi.write_pin(ii, 1)
+            time.sleep(run_time)
+            iopi.write_pin(ii, 0)
+            time.sleep(interval_time)
+    print("Sequential pumping done.")
+    print()
+
+
+def test_array_pumping(IOPi_address, run_time=10):
+    """Turns all the pumps controlled by the IOPi on for the given amount of time. Perform this test with
+    tubing inlet in water to see if the pumps are working.
+
+    Args:
+        IOPi_address: address of the IOPi
+        run_time: Run time of the pumps in seconds. Defaults to 10.
+    """
+    iopi = IOPi(IOPi_address)
+    for ii in range(1, 17):
+        iopi.set_pin_direction(ii, 0)
+
+    print(f"Performing array {IOPi_address:02X} pumping for {run_time}s.")
+    for ii in range(1, 17):
+        iopi.write_pin(ii, 1)
+
+    time.sleep(run_time)
+
+    for ii in range(1, 17):
+        iopi.write_pin(ii, 0)
+    print("Array pumping done.")
+    print()
+
+
 if __name__ == "__main__":
-    test_I2C_connections()
-    test_ADCPi()
+    # test_I2C_connections()
+    # test_ADCPi()
+    # test_pump_sound()
+    test_array_pumping(0x20, 30)
