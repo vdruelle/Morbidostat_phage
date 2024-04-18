5d1dd96e27bb11563f68100c972e1ddb7441c103
Valentin <valentin.druelle@unibas.ch>
2024-04-18 17:05:11 +0200

    removed old stuff

--- scripts/test.py
+++ scripts/test.py
@@ -8,66 +8,6 @@ from smbus import SMBus
 from test_bench import Interface
 
 
-def test_WS_drift(total_time: float, dt: float = 1, savefile: str = "runs/WS_drift.tsv") -> None:
-    """
-    Measure weight sensor drift and save the data to a TSV file.
-
-    Args:
-        total_time: Total run time in seconds.
-        dt: Time step in seconds.
-        savefile: Path to the file where the data will be saved. Defaults to "runs/WS_drift.tsv".
-    """
-    adc = ADCPi(0x68, 0x69, 14)
-    adc.set_pga(1)
-    adc.set_bit_rate(14)
-
-    WS_current_pin = 1
-    WS_flexi_pin = 2
-
-    times = np.arange(0, total_time, dt)
-    voltages = pd.DataFrame(columns=["time", "WS current", "WS flexi"])
-
-    print("Starting reading...")
-    for ii in range(len(times)):
-        voltages.loc[ii] = [
-            times[ii],
-            np.mean([adc.read_voltage(WS_current_pin) for jj in range(10)]),
-            np.mean([adc.read_voltage(WS_flexi_pin) for jj in range(10)]),
-        ]
-
-        time.sleep(dt)
-        if ii % 10 == 0:
-            print(f"{times[ii]} / {total_time}s")
-    print("Finished reading.")
-
-    # Export the data as a TSV file
-    voltages.to_csv(savefile, sep="\t")
-
-
-def plot_WS_drift(filename: str, scaled: bool = False) -> None:
-    """
-    Load the generated data from a TSV file and plot it using matplotlib.
-
-    Args:
-        filename (str): Path to the TSV file containing the data.
-
-    Returns:s
-        None
-    """
-    data = pd.read_csv(filename, sep="\t")
-    if scaled:
-        data["WS current"] = data["WS current"] / max(data["WS current"])
-        data["WS flexi"] = data["WS flexi"] / max(data["WS flexi"])
-
-    plt.figure()
-    plt.plot(data["time"], data["WS current"], label="WS current")
-    plt.plot(data["time"], data["WS flexi"], label="WS flexi")
-    plt.xlabel("Time [s]")
-    plt.ylabel("Voltage (scaled) [V]")
-    plt.legend()
-    plt.grid()
-
-
 def test_capacitive_data():
     """Test capacitive reading"""
 
@@ -138,8 +78,9 @@ def test_capacitive_measure(sampling_time=0.1):
     CH1_SETUP_VALUE = 195  # shorter average, usually pretty good
     CONFIGURATION_VALUE = 49
 
-    tca = 0x71
-    bus.write_byte(tca, 1 << 7)
+    # Mux switching
+    tca = 0x70
+    bus.write_byte(tca, 1 << 0)
 
     # Setup AD7150
     write_register(AD7150_I2C_ADDRESS, AD7150_REG_CH1_SETUP, CH1_SETUP_VALUE)
@@ -186,52 +127,6 @@ def test_capacitive_measure(sampling_time=0.1):
         print("Program stopped by user.")
         plt.close()
         bus.close()
-
-
-def i2c_scan(bus):
-    """
-    Scan for I2C devices on the bus and print out their addresses.
-
-    Args:
-        None
-
-    Returns:
-        None
-    """
-    addresses = []
-    for device in range(128):
-        try:
-            bus.read_byte(device)
-            address = "0x%02x" % device
-            addresses += [address]
-        except IOError:
-            pass
-    time.sleep(0.1)
-    return addresses
-
-
-def detect_new_I2C(bus, existing=["0x20", "0x21", "0x22", "0x23", "0x68", "0x69", "0x6a", "0x6b", "0x70", "0x71"]):
-    "Scan for I2C devices and check for devices other than the existing ones"
-    addresses = i2c_scan(bus)
-
-    for address in addresses:
-        if address not in existing:
-            print(address)
-
-
-def scan_mux_addresses(bus, tca=0x70):
-    """
-    Scan for capacitive sensors on the mux bus and print out their channel.
-    """
-    for ii in range(0, 7):
-        bus.write_byte(tca, 1 << ii)  # This is how you switch channel
-        if "0x48" in i2c_scan(bus):
-            print(f"Channel {ii} detected")
-        else:
-            print(f"Channel {ii} not detected")
-
-    # Reset the mux
-    bus.write_byte(tca, 0)
 
 
 def switching_speed(bus, tca):
